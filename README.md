7b188157cda9894daffe0f885426ea72f43dc7ef
palmcivet7 <palmcivet@proton.me>
2023-04-09 22:00:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ICO Test Project
+
+This project was made following along with an LW3 lesson. It includes a front end and a back end.
---
+++ back-end/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xd486a0e908582b25707df2adc0a350e77d6c9f9b";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
